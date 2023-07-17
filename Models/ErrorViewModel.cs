ddb73198ce6a0d82ed82ce7d8ed0041853ae85d8
HungLingHua <memory3993@gmail.com>
2023-07-17 19:48:54 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AjaxHMK.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
